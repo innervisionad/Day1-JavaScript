e43a8ca52a5565378762a42ba6620191a06191e6
Adam Clements <aj.clements@live.co.uk>
2016-06-13 16:25:11 +0100

    Task 9

---
+++ Task9.js
@@ -0,0 +1,28 @@
+
+var event1 = document.getElementById("button1");
+event1.addEventListener('click',add,false);
+
+function add(){
+var userinput1 = document.getElementById("input1").value;
+var userinput2 = document.getElementById("input2").value;
+var userinput3 = document.getElementById("input3").value;
+var int1 = parseInt(userinput1);
+var int2 = parseInt(userinput2);
+var int3 = parseInt(userinput3);
+
+var calc1 = (int1 + int2) * int3;
+var calc2 = int1 + int2 + int3;
+
+var output = calc1 - calc2;    
+    
+    
+//var total = parseInt(userinput1) + parseInt(userinput2);  
+
+//var upNum = int1 + 1;
+//var downNum = int1 - 1;
+
+    
+document.getElementById("difference").innerHTML = output;
+
+}
+  
