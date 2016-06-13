8e413f36c4c6d14df0a74df9945b7e2c1ef36a8a
Adam Clements <aj.clements@live.co.uk>
2016-06-13 15:04:36 +0100

    separatedJs

---
+++ simplejs.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',myfirst,false);
+var event1 = document.getElementById("button1");
+event1.addEventListener('click',myfirst,false);
+function myfirst(){
+//Get user input 1
+var userinput1 = document.getElementById("input1").value;
+//Write output 1 onto webpage
+document.getElementById("demo").innerHTML = userinput1;
+}
+  
