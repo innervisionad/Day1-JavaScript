b944d24a12a75ba28282d2bfbcf8bcdac2b400f5
Adam Clements <aj.clements@live.co.uk>
2016-06-13 15:10:36 +0100

    simplejs

--- simplejs.js
+++ simplejs.js
@@ -1,4 +1,5 @@
-window.addEventListener('load',myfirst,false);
+
+//EVENT LISTENER
 var event1 = document.getElementById("button1");
 event1.addEventListener('click',myfirst,false);
 function myfirst(){
