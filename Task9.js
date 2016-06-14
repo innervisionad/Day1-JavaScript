3f8937387af278837af7c53925381bc439a93ee2
Adam Clements <aj.clements@live.co.uk>
2016-06-14 09:44:19 +0100

    Working Task9

--- Task9.js
+++ Task9.js
@@ -1,19 +1,26 @@
-
+//Collect a number from the user and display on the webpage if the number is negative, less than 10 or more than 10
 var event1 = document.getElementById("button1");
 event1.addEventListener('click',add,false);
 
 function add(){
 var userinput1 = document.getElementById("input1").value;
-var userinput2 = document.getElementById("input2").value;
-var userinput3 = document.getElementById("input3").value;
 var int1 = parseInt(userinput1);
-var int2 = parseInt(userinput2);
-var int3 = parseInt(userinput3);
 
-var calc1 = (int1 + int2) * int3;
-var calc2 = int1 + int2 + int3;
+if(int1 <10 && int1>=0){
+    output = "It's less than 10!";
+}
+else if(int1 > 10){
+    output = "It's greater than 10!";
+}
+else if(int1<0){
+    output = "It's a negative!";
+}
+else {
+     output = "It's 10!";
+}
+   
 
-var output = calc1 - calc2;    
+//var output = calc1 - calc2;    
     
     
 //var total = parseInt(userinput1) + parseInt(userinput2);  
